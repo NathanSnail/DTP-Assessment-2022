b922a025be3392fe61f493ea94ce1e201c32b35a
nathan <nathan.titirangi@gmail.com>
2022-06-08 13:44:08 +1200

    started on combat system

---
+++ Dino.cs
@@ -0,0 +1,24 @@
+namespace DTP_Assessment_2022
+{
+    public abstract class Dino
+    {
+        //health change after attack = dmg * ln(attack) - defense
+        //special case if dmg < 0 ie healing move
+        public int maxHealth;
+        public float health;
+        public int defense;
+        public int attack;
+        public string name;
+        public Attack[] attacks = new Attack[4];
+        public Dino(int maxHealth, int attack, int defense, string name)
+        {
+            this.maxHealth = maxHealth;
+            this.health = maxHealth;
+            this.attack = attack;
+            this.defense = defense;
+            this.name = name;
+        }
+        public void takeDamage(float amount) => health -= amount > 0 ? Math.Max(amount - defense,0) : amount;
+        public float getDamage(float amount) => amount > 0 ? amount * (float)Math.Log(Math.Max(attack,0)+2) : amount;
+    }
+}
---
+++ GameManager.cs
@@ -0,0 +1,8 @@
+namespace DTP_Assessment_2022
+{
+    public static class GameManager
+    {
+        public static Dino enemy;
+        public static Dino player;
+    }
+}
